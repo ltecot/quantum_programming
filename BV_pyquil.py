748d1da0fe16fd0f815265093138728ffd476cab
Victor Fu <vfu-13@peddie.org>
2019-05-09 17:23:23 -0700

    BV Outside circuit

--- BV_pyquil.py
+++ BV_pyquil.py
@@ -13,4 +13,23 @@ B = random.randint(0,1)
 print(A)
 print(B)
 
+bv.inst(X(0))
+bv.inst(H(0))
+
+for qubit in range(1, N+1):
+    bv.inst(H(qubit))
+
+
+bv.inst(("U_f",)
+
+for qubit in range(1, N+1):
+    bv.inst(H(qubit))
+
+
+
+qc = get_qc('1q-qvm')  # You can make any 'nq-qvm' this way for any reasonable 'n'
+executable = qc.compile(bv)
+result = qc.run_and_measure(executable, trails = 50)
+print(result)
+
 
